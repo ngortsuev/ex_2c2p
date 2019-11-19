a2914246405086c4e7f0b8b1dd825e112c42b42a
ngortsuev <n.gortsuev@outlook.com>
2019-11-20 00:39:10 +0300

    Add filter for view transactions

--- TransactionService/TransactionService/Controllers/TransactionsController.cs
+++ TransactionService/TransactionService/Controllers/TransactionsController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using TransactionService.Domain;
+using TransactionService.ViewModels;
 
 namespace TransactionService.Controllers
 {
@@ -16,9 +18,22 @@ namespace TransactionService.Controllers
             db = transactionDb;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(TransactionsViewModel model)
         {
-            return View(db.Transactions);
+            if(model == null)
+                model = new TransactionsViewModel();
+
+            if (model.SelectedStatus != null)
+            {
+                model.List = db.Transactions.Where(t => t.Status == model.SelectedStatus);
+            }
+            else
+            {
+                model.List = db.Transactions;
+            }
+            model.StatusList = new SelectList(db.Transactions.Select(c => new { Name = c.Status }).Distinct().ToList(), "Name", "Name");
+            model.CurrencyList = new SelectList(db.Transactions.Select(c => new { Name = c.CurrencyCode }).Distinct().ToList(), "Name", "Name");
+            return View(model);
         }
     }
 }
---
+++ TransactionService/TransactionService/ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TransactionService.Models;
+
+namespace TransactionService.ViewModels
+{
+    public class TransactionsViewModel
+    {
+        public IEnumerable<Transaction> List { get; set; }
+        public bool CurrencyFlag { get; set; }
+        public bool DateFlag { get; set; }
+        public bool StatusFlag { get; set; }
+        public string SelectedCurrency { get; set; }
+        public string SelectedDate { get; set; }
+        public string SelectedStatus { get; set; }
+        
+        public SelectList CurrencyList { get; set; }
+        public SelectList StatusList { get; set; }
+    }
+}
